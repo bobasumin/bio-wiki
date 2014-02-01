a7f7445e7d8353b6cda982807652c257a7c95243
Soomin <bobasumin@gmail.com>
2014-02-01 14:01:14 -0500

    Updated README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bio-wiki
+
+A wikipedia-style application that allows users to create their own personal wiki-bio pages and share them publicly or privately with other users.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BioWiki::Application.config.secret_token = 'b34c749d8085c3b983e1fb1ea64eeaa1c242f6341454a23de718df215071c23b12f944e1b68c62ce1617b9fa7d80482adf0741dd36918c2dbaa13a1092ec47c8'
