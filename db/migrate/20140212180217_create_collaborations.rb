ca86b3c6093c73180db245ebe53be397f040e91b
Soomin <bobasumin@gmail.com>
2014-02-18 23:22:45 -0500

    clearing merge error

--- db/migrate/20140212180217_create_collaborations.rb
+++
@@ -1,9 +0,0 @@
-class CreateCollaborations < ActiveRecord::Migration
-  def change
-    create_table :collaborations do |t|
-      t.belongs_to :post
-      t.belongs_to :user
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,25 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140217033800) do
-
-  create_table "collaborations", :force => true do |t|
-    t.integer  "post_id"
-    t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "friendly_id_slugs", :force => true do |t|
-    t.string   "slug",                         :null => false
-    t.integer  "sluggable_id",                 :null => false
-    t.string   "sluggable_type", :limit => 40
-    t.datetime "created_at"
-  end
-
-  add_index "friendly_id_slugs", ["slug", "sluggable_type"], :name => "index_friendly_id_slugs_on_slug_and_sluggable_type", :unique => true
-  add_index "friendly_id_slugs", ["sluggable_id"], :name => "index_friendly_id_slugs_on_sluggable_id"
-  add_index "friendly_id_slugs", ["sluggable_type"], :name => "index_friendly_id_slugs_on_sluggable_type"
+ActiveRecord::Schema.define(:version => 20140217031923) do
 
   create_table "plans", :force => true do |t|
     t.string   "name"
@@ -38,23 +20,6 @@ ActiveRecord::Schema.define(:version => 20140217033800) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
-
-  create_table "posts", :force => true do |t|
-    t.text     "summary"
-    t.text     "earlylife"
-    t.text     "career"
-    t.text     "personallife"
-    t.integer  "birthdate"
-    t.boolean  "public",       :default => true
-    t.datetime "created_at",                     :null => false
-    t.datetime "updated_at",                     :null => false
-    t.integer  "user_id"
-    t.string   "slug"
-    t.string   "image"
-  end
-
-  add_index "posts", ["slug"], :name => "index_posts_on_slug"
-  add_index "posts", ["user_id"], :name => "index_posts_on_user_id"
 
   create_table "subscriptions", :force => true do |t|
     t.integer  "user_id"
