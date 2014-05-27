83b8872dea532f8569985b8521bbcf949b74ff07
Soomin <bobasumin@gmail.com>
2014-05-27 19:56:19 -0400

    friendship model migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140307153911) do
+ActiveRecord::Schema.define(:version => 20140319194344) do
 
   create_table "biocards", :force => true do |t|
     t.date     "dob"
